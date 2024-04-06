07d86c46b07bc4a6b23e508b9a541b823111ad04
vogel <over.castrillon22396@ucaldas.edu.co>
2024-04-06 17:37:44 -0500

    Refactor and repositioning elements

--- app/views/menu.py
+++ app/views/menu.py
@@ -29,11 +29,29 @@ class Menu:
 
     """ Specialized methods """
 
+
+class Menu:
+    ''' Class Menu is used to render to user. '''
+
+    def __init__(self) -> None:
+        self._G: MultiDiGraph = MultiDiGraph()
+        self.gService: GraphService = GraphService(self._G)
+        self.aService: Algorithm = Algorithm(self._G)
+        self.gifs_generated = False  # State to track if GIFs are already generated
+
+    def set_graph(self, graph: MultiDiGraph) -> None:
+        self._G = graph
+        self.gService.set_graph(graph)
+        self.aService.set_graph(graph)
+
+    """ Specialized methods """
+
     def render(self):
         st.set_page_config(layout="wide")
 
         # Cargar datos del mapa
-        barrios: list[MultiDiGraph] = self.gService.load_neighborhoods(NOMBRES_BARRIOS)
+        barrios: list[MultiDiGraph] = self.gService.load_neighborhoods(
+            NOMBRES_BARRIOS)
         st.title('Selecciona un barrio para mostrar su gráfico:')
         barrio_selec = st.selectbox('Barrios', NOMBRES_BARRIOS)
         mapa, graph = self.gService.load_neighbor_data(barrio_selec, barrios)
@@ -41,69 +59,38 @@ class Menu:
         self.gService.reset_graph()
 
         # Mostrar mapa en una columna y campos de entrada en la otra
-        col1, col2 = st.columns([3, 2])  # Ajustar anchos de columnas según necesidad
+        # Ajustar anchos de columnas según necesidad
+        col1, col2 = st.columns([3, 2])
         with col1:
             self.graficar_mapa(mapa)
 
         with col2:
             st.title('Encuentra tu ruta')
-            origen = st.text_input('Origen', '')
             destino = st.text_input('Destino', '')
+            origen = st.text_input('Origen', '')
             if (origen != '' and destino != ''):
                 origen, destino = int(origen), int(destino)
                 if st.button('Hallar ruta más corta'):
                     self.shortest_path(origen, destino)
+                    self.gifs_generated = False  # Reset the state after calculating the route
 
         # Espacio entre secciones
         st.write('---')
 
-        # Mostrar animaciones en una fila
-        col3, col4 = st.columns(2)
-        with col3:
-            st.title('Animación del algoritmo A*')
-            st.image(f'{IMAGES_DIR}/animation.gif', caption='Animación del algoritmo A*')
+        # Mostrar animaciones en una fila only if GIFs are not already generated
+        if not self.gifs_generated:
+            col3, col4 = st.columns(2)
+            with col3:
+                st.title('Animación del algoritmo A*')
+                st.image(f'{IMAGES_DIR}/animation.gif',
+                         caption='Animación del algoritmo A*')
 
-        with col4:
-            st.title('Animación del camino más corto')
-            st.image(f'{ROAD_DIR}/image_animation.gif', caption='Animación del camino más corto')
+            with col4:
+                st.title('Animación del camino más corto')
+                st.image(f'{ROAD_DIR}/image_animation.gif',
+                         caption='Animación del camino más corto')
 
-
-    # def rendere(self):
-    #     st.set_page_config(layout="wide")
-
-    #     barrios: list[MultiDiGraph] = self.gService\
-    #         .load_neighborhoods(NOMBRES_BARRIOS)
-
-    #     st.title('Selecciona un barrio para mostrar su gráfico:')
-    #     barrio_selec = st.selectbox('Barrios', NOMBRES_BARRIOS)
-
-    #     mapa, graph = self.gService.load_neighbor_data(barrio_selec, barrios)
-    #     self.set_graph(graph)
-
-    #     self.gService.reset_graph()
-
-    #     self.graficar_mapa(mapa)
-
-    #     st.title('Encuentra tu ruta')
-    #     col1, col2 = st.columns(2)
-
-    #     with col1:
-    #         destino = st.text_input('Destino', '')
-    #     with col2:
-    #         origen = st.text_input('Origen', '')
-
-    #     if (origen != '' and destino != ''):
-    #         origen, destino = int(origen), int(destino)
-    #         if st.button('Hallar ruta más corta'):
-    #             self.shortest_path(origen, destino)
-    #         # elif st.button('Hallar ruta más rapida'):
-    #         #     self.fastest_path(origen, destino)
-    #         # elif st.button('Hallar ruta con menor consumo de combustible'):
-    #         #     self.less_fuel_path(origen, destino)
-    #         # elif st.button('Hallar ruta más económica para el pasajero'):
-    #         #     self.less_cost_path(origen, destino)
-    #         # elif st.button('Hacer un Tour trip'):
-    #         #     self.tour_trip(origen, destino)
+            self.gifs_generated = True  # Set the state to True after showing the GIFs
 
     def shortest_path(self, origen: int, destino: int):
         self.limpiar_carpeta()
@@ -121,26 +108,24 @@ class Menu:
         ]
 
         # Crear el GIF del proceso del algoritmo A*
-        animation_path = f'{IMAGES_DIR}/animation.gif'
-        imageio.mimsave(animation_path,
-                        [imageio.imread(file) for file in tree_files],
-                        fps=3)
-        st.title('Animación del algoritmo')
-        st.image(animation_path, caption='Animación del algoritmo A*')
+        if len(tree_files) > 0:
+            animation_path = f'{IMAGES_DIR}/animation.gif'
+            imageio.mimsave(animation_path,
+                            [imageio.imread(file) for file in tree_files],
+                            fps=3)
 
         # Obtener las imágenes del camino reconstruido
         route_files = [
             f'{ROAD_DIR}/image_{i}.png'
-            for i in range(len(os.listdir(ROAD_DIR)) - 1)
+            for i in range(len(os.listdir(ROAD_DIR)))
         ]
 
         # Crear el GIF del camino reconstruido
-        road_animation_path = f'{ROAD_DIR}/image_animation.gif'
-        imageio.mimsave(road_animation_path,
-                        [imageio.imread(file) for file in route_files],
-                        fps=3)
-        st.title('Animación del camino más corto')
-        st.image(road_animation_path, caption='Animación del camino más corto')
+        if len(route_files) > 0:
+            road_animation_path = f'{ROAD_DIR}/image_animation.gif'
+            imageio.mimsave(road_animation_path,
+                            [imageio.imread(file) for file in route_files],
+                            fps=3)
 
     def fastest_path(self, origen: int, destino: int):
         pass
